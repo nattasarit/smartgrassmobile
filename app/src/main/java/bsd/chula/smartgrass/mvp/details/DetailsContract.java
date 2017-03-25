525290787af2c4f1b8afec78e3552efb97c9e383
Kittisak Buaphanna <kurosaki_tisak@hotmail.com>
2017-03-25 15:13:46 +0700

    no message

---
+++ app/src/main/java/bsd/chula/smartgrass/mvp/details/DetailsContract.java
@@ -0,0 +1,21 @@
+package bsd.chula.smartgrass.mvp.details;
+
+/**
+ * Created by Dev_Tee on 3/25/17.
+ */
+
+public interface DetailsContract {
+
+    interface View {
+
+        void showTaskDetails();
+    }
+
+    interface UserActions {
+
+    }
+
+    interface Interactor {
+
+    }
+}
